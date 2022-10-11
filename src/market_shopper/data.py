e6f99d93b45c480686308e3e785afa2676b724ea
Tia Burton <tiaaburton@me.com>
2022-10-10 23:40:39 -0700

    Including login, registration, and a quickstart Dockerfile.

--- src/market_shopper/data.py
+++ src/market_shopper/data.py
@@ -2,19 +2,26 @@ import pandas as pd
 import numpy as np
 import os
 
+
 class GetData:
     def __init__(self):
         self.data = {}
 
-    def from_finnhub(self):
+    def from_yfinance(self, ticker):
         return None
 
-    def from_SEC(self):
+    def from_SEC(self, ticker):
         return None
 
-    def from_FRED(self):
+    def from_FRED(self, ticker):
         return None
+
+    def from_reddit(self, ticker, sub):
+        return None
+
+    def from_twitter(self, ticker):
+        return None
+
 
 if __name__ == '__main__':
-    print(GetData)
-
+    ...
