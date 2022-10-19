c744963cccd3e3c6085868479a28506a749d7de3
Joshua Raymond <joshuaaraymond@gmail.com>
2022-10-19 17:26:30 -0400

    First working version of Google SSO

--- src/data/mongo.py
+++ src/data/mongo.py
@@ -8,7 +8,7 @@ def get_mongo_connection():
     # config.read('D:\Documents\MarketShoppers\MADS_Capstone\src\data\config.ini')
     # script_dir = os.path.dirname(__file__)
     # config.read(os.path.join(script_dir, 'config.ini'))
-    config.read('../config.ini')
+    config.read('config.ini')
     mongo_config = config['MONGO']
     # mongo_connection = "mongodb+srv://" + mongo_config['User'] + ":" + mongo_config['Password'] + "@" + mongo_config['Address'] + ":" + mongo_config['port'] + "/?authSource=admin"
     mongo_connection = "mongodb+srv://" + mongo_config['User'] + ":" + mongo_config['Password'] + "@" + mongo_config['Address'] + "/test"
