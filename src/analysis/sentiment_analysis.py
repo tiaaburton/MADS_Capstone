e3a1ce25e800354012ffc8494c50ca9fbcb4a255
Lint Action <lint-action@samuelmeuli.com>
2022-10-28 18:27:20 +0000

    Fix code style issues with Black

--- src/analysis/sentiment_analysis.py
+++ src/analysis/sentiment_analysis.py
@@ -3,22 +3,21 @@ from flask.blueprints import Blueprint
 import requests
 import praw
 
-bp = Blueprint('sentiment', __name__, url_prefix='/sentiment')
+bp = Blueprint("sentiment", __name__, url_prefix="/sentiment")
 
 
-@bp.route('/social_credentials')
+@bp.route("/social_credentials")
 def store_social_credentials():
-    session['reddit_client_id'] = request.form['reddit_client']
-    session['reddit_secret_key'] = request.form['reddit_secret']
-    session['twitter_client_id'] = request.form['twitter_client']
-    session['twitter_secret_key'] = request.form['twitter_secret']
-    return redirect(url_for('analysis'))
+    session["reddit_client_id"] = request.form["reddit_client"]
+    session["reddit_secret_key"] = request.form["reddit_secret"]
+    session["twitter_client_id"] = request.form["twitter_client"]
+    session["twitter_secret_key"] = request.form["twitter_secret"]
+    return redirect(url_for("analysis"))
 
 
 def initialize_reddit():
     reddit = praw.Reddit(
-        client_id=session['reddit_client_id'],
-        client_secret=session['reddit_secret_key'],
-        user_agent="Sentiment Analysis by Market Shopper (u/tiaaburton)"
+        client_id=session["reddit_client_id"],
+        client_secret=session["reddit_secret_key"],
+        user_agent="Sentiment Analysis by Market Shopper (u/tiaaburton)",
     )
-
