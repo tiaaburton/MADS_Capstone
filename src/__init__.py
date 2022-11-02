c5703fb724a98aa95a378f12abf6e2e39dc14c15
Joshua Raymond <joshuaaraymond@gmail.com>
2022-11-01 21:45:53 -0400

    Working version of Google SSO button

--- src/__init__.py
+++ src/__init__.py
@@ -26,6 +26,9 @@ from plaid.model.investments_holdings_get_request import InvestmentsHoldingsGetR
 from plaid.model.products import Products
 from src.server import get_plaid_client, request_institutions
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 import src.auth
 import src.db
 import src.server
@@ -114,83 +117,122 @@ def create_app(test_config=None):
         # if request.method == 'POST':
         #     session['username'] = request.form['username']
         #     return redirect(url_for('index'))
-        # return render_template('auth/login.html')
+        return render_template('auth/login.html')
 
-        ### Google SSO Code ###
-        # Find out what URL to hit for Google login
-        google_provider_cfg = get_google_provider_cfg()
-        authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+        # ### Google SSO Code ###
+        # # Find out what URL to hit for Google login
+        # google_provider_cfg = get_google_provider_cfg()
+        # authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-        # Use library to construct the request for Google login and provide
-        # scopes that let you retrieve user's profile from Google
-        request_uri = client.prepare_request_uri(
-            authorization_endpoint,
-            redirect_uri=f"{request.base_url}/callback",
-            scope=["openid", "email", "profile"],
-        )
-        return redirect(request_uri)
+        # # Use library to construct the request for Google login and provide
+        # # scopes that let you retrieve user's profile from Google
+        # request_uri = client.prepare_request_uri(
+        #     authorization_endpoint,
+        #     redirect_uri=f"{request.base_url}/callback",
+        #     scope=["openid", "email", "profile"],
+        # )
+        # return redirect(request_uri)
 
-    @app.route("/login/callback")
+    @app.route("/login/callback", methods=['POST'])
     def callback():
-        # Get authorization code Google sent back to you
-        code = request.args.get("code")
-        # Find out what URL to hit to get tokens that allow you to ask for
-        # things on behalf of a user
-        google_provider_cfg = get_google_provider_cfg()
-        token_endpoint = google_provider_cfg["token_endpoint"]
-        # Prepare and send a request to get tokens! Yay tokens!
-        token_url, headers, body = client.prepare_token_request(
-            token_endpoint,
-            authorization_response=request.url,
-            redirect_url=request.base_url,
-            code=code,
-        )
-        token_response = requests.post(
-            token_url,
-            headers=headers,
-            data=body,
-            auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-        )
+        # # Get authorization code Google sent back to you
+        # code = request.args.get("code")
+        # # Find out what URL to hit to get tokens that allow you to ask for
+        # # things on behalf of a user
+        # google_provider_cfg = get_google_provider_cfg()
+        # token_endpoint = google_provider_cfg["token_endpoint"]
+        # # Prepare and send a request to get tokens! Yay tokens!
+        # token_url, headers, body = client.prepare_token_request(
+        #     token_endpoint,
+        #     authorization_response=request.url,
+        #     redirect_url=request.base_url,
+        #     code=code,
+        # )
+        # token_response = requests.post(
+        #     token_url,
+        #     headers=headers,
+        #     data=body,
+        #     auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+        # )
 
-        # Parse the tokens!
-        client.parse_request_body_response(json.dumps(token_response.json()))
+        # # Parse the tokens!
+        # client.parse_request_body_response(json.dumps(token_response.json()))
 
-        # Save the token to help log out
-        session["state"] = client.state
+        # # Save the token to help log out
+        # session["state"] = client.state
 
-        # Now that you have tokens (yay) let's find and hit the URL
-        # from Google that gives you the user's profile information,
-        # including their Google profile image and email
-        userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-        uri, headers, body = client.add_token(userinfo_endpoint)
-        userinfo_response = requests.get(uri, headers=headers, data=body)
+        # # Now that you have tokens (yay) let's find and hit the URL
+        # # from Google that gives you the user's profile information,
+        # # including their Google profile image and email
+        # userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+        # uri, headers, body = client.add_token(userinfo_endpoint)
+        # userinfo_response = requests.get(uri, headers=headers, data=body)
 
-        # You want to make sure their email is verified.
-        # The user authenticated with Google, authorized your
-        # app, and now you've verified their email through Google!
-        if userinfo_response.json().get("email_verified"):
-            unique_id = userinfo_response.json()["sub"]
-            users_email = userinfo_response.json()["email"]
-            picture = userinfo_response.json()["picture"]
-            users_name = userinfo_response.json()["given_name"]
-        else:
-            return "User email not available or not verified by Google.", 400
+        # # You want to make sure their email is verified.
+        # # The user authenticated with Google, authorized your
+        # # app, and now you've verified their email through Google!
+        # if userinfo_response.json().get("email_verified"):
+        #     unique_id = userinfo_response.json()["sub"]
+        #     users_email = userinfo_response.json()["email"]
+        #     picture = userinfo_response.json()["picture"]
+        #     users_name = userinfo_response.json()["given_name"]
+        # else:
+        #     return "User email not available or not verified by Google.", 400
 
-        # Create a user in your db with the information provided
-        # by Google
-        user = User(
-            id_=unique_id, name=users_name, email=users_email, profile_pic=picture
-        )
+        # # Create a user in your db with the information provided
+        # # by Google
+        # user = User(
+        #     id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+        # )
 
-        # Doesn't exist? Add it to the database.
-        if not User.get(unique_id):
-            User.create(unique_id, users_name, users_email, picture)
+        # # Doesn't exist? Add it to the database.
+        # if not User.get(unique_id):
+        #     User.create(unique_id, users_name, users_email, picture)
 
-        # Begin user session by logging the user in
-        login_user(user)
+        # # Begin user session by logging the user in
+        # login_user(user)
 
-        # Send user to portfolio manager to start using entering credentials to unlock
-        # a feature within the app
+        # # Send user to portfolio manager to start using entering credentials to unlock
+        # # a feature within the app
+
+        ### Begin modified code ###
+
+        if request.method == "POST":
+        
+            csrf_token_cookie = request.cookies.get('g_csrf_token')
+            token = request.form.get('credential') 
+            if not csrf_token_cookie:
+                webapp2.abort(400, 'No CSRF token in Cookie.')
+            # csrf_token_body = request.get('g_csrf_token')
+            # csrf_token_body = request.args.get('g_csrf_token', '')    
+            # if not csrf_token_body:
+            #     webapp2.abort(400, 'No CSRF token in post body.')
+            # if csrf_token_cookie != csrf_token_body:
+            #     webapp2.abort(400, 'Failed to verify double submit cookie.')
+
+            try:
+                # Specify the CLIENT_ID of the app that accesses the backend:
+                idinfo = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+
+                # ID token is valid. Get the user's Google Account ID from the decoded token.
+                unique_id = idinfo['sub']
+                users_name = idinfo['name']
+                users_email = idinfo['email']
+                picture = idinfo['picture']
+            except ValueError:
+                # Invalid token
+                raise Exception("Invalid Google OAuth token: " + str(csrf_token_cookie))
+
+            user = User(
+                id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+            )
+
+            if not User.get(unique_id):
+                User.create(unique_id, users_name, users_email, picture)
+
+            # Begin user session by logging the user in
+            login_user(user)
+
         return redirect(url_for("index"))
 
     @app.route("/logout")
